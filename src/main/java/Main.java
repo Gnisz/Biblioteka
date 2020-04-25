888c2e8775c0125a7bb42f23df090545c8a51f27
Gnisz <dominik.mielczarek.dm@gmail.com>
2020-04-25 10:29:34 +0200

    Wstępny UI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner SCANNER;
+
     public static void main(String[] args) {
-        System.out.println("Robimy bibioteke");
+
+        SCANNER = new Scanner(System.in);
+
+        System.out.println("Witaj w Bibliotece! Co mogę dla ciebie zrobić?");
+
+
+
+        while(true){
+
+            System.out.println("1) Wyświetl wszystkie książki\n" +
+                    "2) Dodaj książkę\n" +
+                    "3) usuń książkę\n" +
+                    "Wybieram");
+
+            int wpisywanie = SCANNER.nextInt();
+
+            switch (wpisywanie){
+                case 1:
+                    System.out.println(" [Wyświetl wszystkie ksiązki] jeszcze nie zaimplementowana");
+                    break;
+                case 2:
+                    System.out.println(" [Dodaj ksiązkę] jeszcze nie zaimplementowana");
+                    break;
+                case 3:
+                    System.out.println(" [Usuń ksiązkę] jeszcze nie zaimplementowana");
+                    break;
+                default:
+                    System.out.println("bledne dane");
+                    break;
+            }
+        }
     }
 
 }
